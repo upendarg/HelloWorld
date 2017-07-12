2c6618ec4641ce9a114ba3702a054fcf99b47a25
upendarR <upendarhyd@yahoo.co.in>
2017-07-12 22:47:54 +0100

    helloworld java program

---
+++ Jenkins.java
@@ -0,0 +1,14 @@
+package hello;
+
+public class Jenkins {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Printing numbers from 1 to 10");
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
